6a2ea25aac99988cdea96f271adcae6f78c84dd5
GUSTAVO_ALBUQUERQUE_LOURENCO_MATTOS_DE_CASTRO <22302093@ALFAF17.FLORESTA>
2024-10-09 08:44:00 -0300

    commit5

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.getElementsByClassName('liPaginas').addEventListener('click', function() {
-    document.getElementById('nome').scrollIntoView({ behavior: 'smooth' });
-});
+// document.getElementsByClassName('liPaginas').addEventListener('click', function() {
+//     document.getElementById('nome').scrollIntoView({ behavior: 'smooth' });
+// });
+
+var i = 0;
+var i2 = 0;
+var txt = 'Desenvolvedor Front End'; /* The text */
+var txt2 = 'GUSTAVO ALBQUERQUE'; /* The text */
+var txt3 = 'OLÁ, EU SOU'
+
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("digitando").innerHTML += txt.charAt(i);
+    document.getElementById("gustavo").innerHTML += txt2.charAt(i);
+    document.getElementById("ola").innerHTML += txt3.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
